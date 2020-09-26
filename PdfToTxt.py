c3c723c93d8da0f0d486eb7f693f4d1f3d141b88
MeganWu <31881597+Meganwu@users.noreply.github.com>
2020-09-26 20:33:57 +0800

    Update PdfToTxt.py

--- PdfToTxt.py
+++ PdfToTxt.py
@@ -24,8 +24,36 @@ def PdfTotxt(pdfname,outfile='outfile.txt'):
             f.write(text)
 
 def ExtractIupac(txtname,outfile='outfile.csv'):
-    with open(txtname,encoding='utf-8') as f_origin:
-         for line in f_origin:
+     rc=re.compile(r'^[aA-Z"0\>\-\\\)/\'’”]')
+     rb=re.compile(r'^[0-9][0-9%]')
+     ra=re.compile(r'^\w$')
+     f2 = open(ourfile, 'w', encoding='utf-8')   
+     with open(txtname,encoding='utf-8') as f_origin:
+       for line in f_origin.readlines():
+          if re.findall(pattern_exampl, line):
+            line = re.sub('^','\n',line)
+            line = re.sub('$\n','  ',line)
+            f2.write(line)
+          else:
+            if rc.findall(line) or rb.findall(line) or ra.findall(line):
+                line = line
+                print(line)
+            else:   
+               line = line.strip()
+               line = re.sub('[\n]+','\n',line)
+               line = re.sub('—','-',line)
+#              line = re.sub('s+','',line)
+               line = re.sub(' ','',line)
+               line = re.sub('I(?<=[\'\-])','1',line)
+               line = re.sub('(?<=-)l','1',line)
+               line = line.split('and')[0]
+               line = re.sub('(?<=[a-zA-Z])1(?<=[a-zA-Z])','l',line)
+               line = re.sub('[Il]RS','1RS',line)
+#               line = re.sub('and(?<=[\(\w+])','',line)
+#             line = ''.join(line.split())
+#              print(line)
+               f2.write(line)    
+      f2.close()           
 
 if __name__ == "__main__":
     patent_id = "US20200048241A1"
